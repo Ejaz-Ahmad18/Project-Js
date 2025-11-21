e2b1ec7d99a0d012a69686e2a87c83dbfa63dae5
ejazahmad18 <numen.ejaz@gmail.com>
2025-11-21 20:30:46 +0530

    On-click-color-change

--- script.js
+++ script.js
@@ -7,5 +7,20 @@
 
 //----Math.random & Math.floor----//
 
-var a  = Math.floor(Math.random()*10)
- console.log(a)
+// var a  = Math.floor(Math.random()*10)
+//  console.log(a)
+
+
+//---- Color Changes by click----//
+
+var box=document.querySelector('#box')
+var btn=document.querySelector('button')
+
+btn.addEventListener("click", function(){
+var a1 =Math.floor(Math.random()*256)
+var a2 =Math.floor(Math.random()*256)
+var a3 =Math.floor(Math.random()*256)
+
+box.style.backgroundColor = `rgb(${a1},${a2},${a3})`
+
+})
