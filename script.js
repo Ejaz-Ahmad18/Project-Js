797b6639bed7e743cdc55b5505ab3e66b73d40a0
ejazahmad18 <numen.ejaz@gmail.com>
2025-11-21 20:08:00 +0530

    Method-Math.floor

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
+
+//----Math.random----//
+
+// var a  = Math.floor(Math.random()*10)
+// console.log(a)
+
+
+//----Math.random & Math.floor----//
+
 var a  = Math.floor(Math.random()*10)
-console.log(a)
+ console.log(a)
