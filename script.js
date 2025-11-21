73ba0fababd3e427689e14c9426c063fcee68b91
ejazahmad18 <numen.ejaz@gmail.com>
2025-11-21 20:36:14 +0530

    Method-Math.floor-using-array

--- script.js
+++ script.js
@@ -13,14 +13,22 @@
 
 //---- Color Changes by click----//
 
-var box=document.querySelector('#box')
-var btn=document.querySelector('button')
+// var box=document.querySelector('#box')
+// var btn=document.querySelector('button')
 
-btn.addEventListener("click", function(){
-var a1 =Math.floor(Math.random()*256)
-var a2 =Math.floor(Math.random()*256)
-var a3 =Math.floor(Math.random()*256)
+// btn.addEventListener("click", function(){
+// var a1 =Math.floor(Math.random()*256)
+// var a2 =Math.floor(Math.random()*256)
+// var a3 =Math.floor(Math.random()*256)
 
-box.style.backgroundColor = `rgb(${a1},${a2},${a3})`
+// box.style.backgroundColor = `rgb(${a1},${a2},${a3})`
 
-})
+// })
+
+//----Math.random & Math.floor Using Array----//
+
+var arr=[1,2,3,4,5,6,78,9,7,9,55,]
+
+var a  = Math.floor(Math.random()*10)
+
+console.log(arr[a])
